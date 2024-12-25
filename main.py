b9c36a63777434fcfa99e745e5660b44b65e4aa6
Sathesh Sivashanmugam <satheshshiva@gmail.com>
2024-12-25 10:37:23 -0700

    ibm granite - working code

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 import torch
 import env
 
-model_name = "mistralai/Mistral-7B-v0.1"
+model_name = "ibm-granite/granite-3.1-8b-instruct"
 cache_dir = './model_cache'
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = "cuda"
 
-model = AutoModelForCausalLM.from_pretrained(model_name,  cache_dir=cache_dir, load_in_4bit=True, torch_dtype=torch.float16, token=env.HF_ACCESS_TOKEN)
-tokenizer = AutoTokenizer.from_pretrained(model_name,  cache_dir=cache_dir, token=env.HF_ACCESS_TOKEN)
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+)
 
-input_text = "Hello, how to learn python code"
-inputs = tokenizer(input_text, return_tensors="pt").to(device)
-outputs = model.generate(inputs["input_ids"], max_length=500, num_return_sequences=1)
+model = AutoModelForCausalLM.from_pretrained(model_name,  
+                                             cache_dir=cache_dir, 
+                                             device_map=device, 
+                                             quantization_config=bnb_config, 
+                                             torch_dtype=torch.bfloat16
+                                             )
+tokenizer = AutoTokenizer.from_pretrained(model_name,  cache_dir=cache_dir)
+model.eval()
 
-print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+# change input text as desired
+chat = [
+    { "role": "user", "content": "Please list one IBM Research laboratory located in the United States. You should only output its name and location." },
+]
+chat = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)
+# tokenize the text
+input_tokens = tokenizer(chat, return_tensors="pt").to(device)
+# generate output tokens
+output = model.generate(**input_tokens, 
+                        max_new_tokens=100)
+# decode output tokens into text
+output = tokenizer.batch_decode(output)
+# print output
+print(output)
