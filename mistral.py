d2e9f4b691a39a09347ec09bc56bf8eebcb07ddd
Sathesh Sivashanmugam <satheshshiva@gmail.com>
2024-12-25 18:37:03 -0700

    single branch

---
+++ mistral.py
@@ -0,0 +1,29 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, pipeline
+import env, torch
+
+model_name = "mistralai/Mistral-7B-Instruct-v0.3"
+cache_dir = './model_cache'
+device = "cuda"
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+)
+
+model = AutoModelForCausalLM.from_pretrained(model_name,  
+                                             cache_dir=cache_dir, 
+                                             token=env.HF_ACCESS_TOKEN,
+                                             quantization_config=bnb_config, 
+                                             torch_dtype=torch.bfloat16
+                                             )
+tokenizer = AutoTokenizer.from_pretrained(model_name,  cache_dir=cache_dir, token=env.HF_ACCESS_TOKEN)
+model.eval()
+
+messages = [
+    {"role": "system", "content": "You are a helpful AI assistant"},
+    {"role": "user", "content": "Write a python code to generate fibonacci series"},
+]
+chatbot = pipeline("text-generation", model=model, tokenizer=tokenizer)
+a=chatbot(messages)
+print(a)
