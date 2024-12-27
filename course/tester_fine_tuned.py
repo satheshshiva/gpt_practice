6a7e93d2aa3764390d06d6615c814c4c150033f7
Sathesh Sivashanmugam <satheshshiva@gmail.com>
2024-12-27 15:37:29 -0700

    fine tuning in course

--- course/tester_fine_tuned.py
+++ course/tester_fine_tuned.py
@@ -2,10 +2,9 @@
 from transformers import  AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
 # Same as before
-checkpoint = "checkpoint-500"
-model_cache_dir = './test-trainer'
-tokenizer = AutoTokenizer.from_pretrained(checkpoint, cache_dir=model_cache_dir )
-model = AutoModelForSequenceClassification.from_pretrained(checkpoint, cache_dir=model_cache_dir,  num_labels=2)
+checkpoint = "fine_tuned_model"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint )
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint,  num_labels=2)
 sequences = [
     "I've been waiting for a HuggingFace course my whole life.",
     "This course is amazing!",
