7374e4be2291a0d292e2cf72df70c83c7718ad58
Sathesh Sivashanmugam <satheshshiva@gmail.com>
2024-12-26 23:59:31 -0700

    removed deprecated words

--- course/nlp_course1.py
+++ course/nlp_course1.py
@@ -21,7 +21,7 @@ def tokenize_function(example):
 tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
-training_args = TrainingArguments("test-trainer", evaluation_strategy="epoch")
+training_args = TrainingArguments("test-trainer", eval_strategy="epoch")
 
 def compute_metrics(eval_preds):
     metric = evaluate.load("glue", "mrpc")
@@ -35,7 +35,7 @@ trainer = Trainer(
     train_dataset=tokenized_datasets["train"],
     eval_dataset=tokenized_datasets["validation"],
     data_collator=data_collator,
-    tokenizer=tokenizer,
+    processing_class=tokenizer,
     compute_metrics=compute_metrics,
 )
 # predictions = trainer.predict(tokenized_datasets["validation"])
