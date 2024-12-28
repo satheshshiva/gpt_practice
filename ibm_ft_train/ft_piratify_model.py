eaf28a4aa45c1be0dab87dd8cafc534d8616759f
Sathesh Sivashanmugam <satheshshiva@gmail.com>
2024-12-28 01:08:52 -0700

    ft - qlora issues

--- ibm_ft_train/ft_piratify_model.py
+++ ibm_ft_train/ft_piratify_model.py
@@ -1,12 +1,14 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, GraniteForCausalLM, GraniteConfig, AutoConfig, pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, TrainingArguments
 import torch
-from datasets import load_dataset
+from datasets import load_dataset,load_from_disk
 import datasets
+from trl import DataCollatorForCompletionOnlyLM, SFTTrainer
+from peft import LoraConfig
+
 
 ## LOADING MODEL
 model_name = "ibm-granite/granite-3.1-2b-instruct"
 model_cache_dir = './model_cache'
-dataset_cache_dir = './dataset_cache'
 device = "cuda"
 
 bnb_config = BitsAndBytesConfig(
@@ -17,59 +19,85 @@ bnb_config = BitsAndBytesConfig(
 
 model = AutoModelForCausalLM.from_pretrained(model_name,  
                                              cache_dir=model_cache_dir, 
-                                             device_map=device, 
+                                            #  device_map=device, 
                                              quantization_config=bnb_config, 
                                              torch_dtype=torch.bfloat16
                                              )
 tokenizer = AutoTokenizer.from_pretrained(model_name,  cache_dir=model_cache_dir)
 model.eval()
 
-## DATA SET PREPARATION
-dataset = load_dataset('alespalla/chatbot_instruction_prompts',
-                       cache_dir=dataset_cache_dir)
 
-def pirateify(batch):
-  prompts = [f"make it sound like a pirate said this, do not include any preamble or explanation only piratify the following: {response}" for response in batch['response']]
-  # Tokenize the inputs in batch and move them to GPU
-  inputs = tokenizer(prompts, return_tensors="pt", padding=True, truncation=True).to('cuda')
-  # Generate the pirate-like responses in batch
-  outputs = model.generate(**inputs, max_new_tokens=256, do_sample=True, top_p=0.95, temperature=0.7)
-  # Decode the generated tokens into text for each output in the batch
-  pirate_responses = []
-  for output in outputs:
-    pr = tokenizer.decode(output, skip_special_tokens=True)
-    if '\n\n' in pr:
-      pirate_responses.append(pr.split('\n\n')[-1])
-    else:
-      pirate_responses.append(pr)
+def reduce_dataset_size():
+  ## Reducing the dataset size for faster training. 
+  train_filtered = pirate_dataset['train'].select(range(5))
+  test_filtered = pirate_dataset['test'].select(range(2))
+  # Save the new dataset
+  return datasets.DatasetDict({
+      'train': train_filtered,
+      'test': test_filtered
+  })
 
-  # Move the outputs back to CPU (to free up GPU memory)
-  inputs = inputs.to('cpu')
-  outputs = outputs.to('cpu')
-  # Clear the GPU cache to release any unused memory
-  torch.cuda.empty_cache()
-  return {
-      'prompt': batch['prompt'],  # The original prompts (already a batch)
-      'response': pirate_responses  # The pirate responses, generated in batch
-  }
+pirate_dataset = load_from_disk('./fine_tuned_dataset/pirate_dataset')
+## comment if not needed
+pirate_dataset = reduce_dataset_size()
 
 
-def filter_long_examples(example):
-    prompt_tokens = tokenizer.tokenize(example['prompt'])
-    response_tokens = tokenizer.tokenize(example['response'])  # Tokenize the response
-    return len(response_tokens) <= 200 and len(prompt_tokens) <= 50
+def formatting_prompts_func(example):
+    output_texts = []
+    for i in range(len(example['prompt'])):
+        text = f"<|system|>\nYou are a helpful assistant\n<|user|>\n{example['prompt'][i]}\n<|assistant|>\n{example['response'][i]}<|endoftext|>"
+        output_texts.append(text)
+    return output_texts
 
-# Apply the filter to both train and test splits
-train_filtered = dataset['train'].select(range(6000)).filter(filter_long_examples)
-test_filtered = dataset['test'].select(range(500)).filter(filter_long_examples)
+response_template = "\n<|assistant|>\n"
 
-print(f"train_filtered: {len(train_filtered)} observations\ntest_filtered: {len(test_filtered)} observations")
-pirate_train = train_filtered.select(range(1500)).map(pirateify, batched=True, batch_size=64)
-pirate_test = test_filtered.select(range(250)).map(pirateify, batched=True, batch_size=64)
 
-# Save the new dataset
-pirate_dataset = datasets.DatasetDict({
-    'train': pirate_train,
-    'test': pirate_test
-})
-pirate_dataset.save_to_disk('./fine_tuned_dataset/pirate_dataset')
+response_template_ids = tokenizer.encode(response_template, add_special_tokens=False)[2:]
+collator = DataCollatorForCompletionOnlyLM(response_template_ids, tokenizer=tokenizer)
+
+
+# Apply qLoRA
+qlora_config = LoraConfig(
+    r=16,  # The rank of the Low-Rank Adaptation
+    lora_alpha=32,  # Scaling factor for the adapted layers
+    target_modules=["q_proj", "v_proj"],  # Layer names to apply LoRA to
+    lora_dropout=0.1,
+    bias="none"
+)
+
+# Initialize the SFTTrainer
+training_args = TrainingArguments(
+    output_dir="./results",
+    learning_rate=2e-4,
+    per_device_train_batch_size=1,
+    per_device_eval_batch_size=1,
+    num_train_epochs=1,
+    logging_steps=100,
+    fp16=True,
+    report_to="none",
+    eval_strategy="steps"
+)
+
+max_seq_length = 250
+
+trainer = SFTTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=pirate_dataset['train'],
+    eval_dataset=pirate_dataset['test'],
+    tokenizer=tokenizer,
+    peft_config = qlora_config,
+    formatting_func=formatting_prompts_func,
+    data_collator=collator,
+    # max_seq_length=max_seq_length,
+)
+# Clear the GPU cache to release any unused memory
+torch.cuda.empty_cache()
+
+## TRAINING
+trainer.train()
+
+## SAVING FINE TUNED MODEL
+model.save_pretrained("./fine_tuned_model/ft_granite_pirateified_qlora")
+tokenizer.save_pretrained("./fine_tuned_model/ft_granite_pirateified_qlora")
+
--- ibm_ft_train/tester_fine_tuned.py
+++ ibm_ft_train/tester_fine_tuned.py
@@ -1,16 +1,34 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, GraniteForCausalLM, GraniteConfig, AutoConfig, pipeline
+import torch
 
-from transformers import  AutoTokenizer, AutoModelForSequenceClassification, pipeline
+model_name = "./fine_tuned_model/ft_granite_pirateified_qlora"
+device = "cuda"
 
-# Same as before
-checkpoint = "./fine_tuned_model/ft_bert-base-uncased"
-# model_cache_dir = ''
-tokenizer = AutoTokenizer.from_pretrained(checkpoint )
-model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
-sequences = [
-    "I've been waiting for a HuggingFace course my whole life.",
-    "This course is amazing!",
-    "I hate this course",
+
+model = AutoModelForCausalLM.from_pretrained(model_name,  
+                                             device_map=device, 
+                                             torch_dtype=torch.bfloat16
+                                             )
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model.eval()
+
+# change input text as desired
+chat = [
+    # { "role": "system", "content": "Knowledge Cutoff Date: April 2024.\nToday's Date: December 25, 2024.\nYou are SecurityGPT, developed by Security Engineering Team at American Express. You are a helpful AI assistant at American Express to help American Express employees with queries related to security tools developed within American Express." },
+    { "role": "user", "content": "who are you?" },
 ]
+chat = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)
 
-pipeline = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
-print(pipeline(sequences))
+prompt = "what is meant by inheritance?"
+# tokenize the text
+input_tokens = tokenizer(prompt, return_tensors="pt").to(device)
+# generate output tokens
+output = model.generate(**input_tokens, 
+                        max_new_tokens=500)
+# decode output tokens into text
+
+pr = tokenizer.decode(output[0], skip_special_tokens=True)
+if '\n\n' in pr:
+    print(pr.split('\n\n')[-1])
+else:
+   print(pr)
